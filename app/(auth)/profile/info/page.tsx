6f12841edd55875df2cbe52b512a9cfe1cf0d254
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2025-02-13 19:17:11 +0300

    feat: add user info page with profile, security, and notification settings; implement UI components for user management

---
+++ app/(auth)/profile/info/page.tsx
@@ -0,0 +1,9 @@
+import UserInfo from '@/components/user/user-info'
+
+export default function UserInfoPage() {
+  return (
+    <div className='flex flex-col items-center justify-center h-full space-y-8'>
+      <UserInfo />
+    </div>
+  )
+}
